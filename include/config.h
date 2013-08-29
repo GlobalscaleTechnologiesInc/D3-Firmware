5991c911341975110bfc43c074c8b5538f954bee
globalscaletechnologiesinc <support@globalscaletechnologies.com>
2013-08-29 13:03:08 -0700

    fix missing

---
+++ include/config.h
@@ -0,0 +1,16 @@
+
+/* Automatically generated - do not edit */
+#include <configs/mv_dove.h>
+#include <asm/config.h>
+#define MV88F6781X0
+#define CONFIG_DOVE_REV_X0
+#define RD_88F6781X0_AVNG
+#define MV_SEC_64K
+#define MV_BOOTSIZE_512K
+#define MV_LARGE_PAGE
+#define MV_DRAM_X0_SUPPORT
+#define MV_DRAM_SAMSUNG_1G
+#define MV_USB
+#define MV_SPI
+#define MV_SPI_BOOT
+#define MV_BOOTROM
---
+++ include/config.mk
@@ -0,0 +1,9 @@
+ARCH   = arm
+CPU    = arm1136
+BOARD  = config_dove
+VENDOR = marvell
+SOC    = pj4
+MV_OUTPUT = rd88ap510avng
+CONFIG_DOVE = y
+DRAM = SAMSUNG_1G
+SPI_BOOT = y
